2a70a303842123576c889e455795e40a11c89860
Christian Patrick Nebab <dongyang0216@gmail.com>
2023-11-09 00:54:33 +0800

    fixing testing

--- src/__test__/LogInPage.test.jsx
+++ src/__test__/LogInPage.test.jsx
@@ -16,31 +16,5 @@ it("render log in page", () => {
   const password = screen.getByPlaceholderText("Password");
   expect(email).toBeInTheDocument();
   expect(password).toBeInTheDocument();
-=======
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { LogInPage } from "../pages/LogInPage";
-import { AccountProvider } from "../Context/AccountContext";
-import { test, expect } from "vitest";
 
-
-test("render log in page", () => {
-  render(
-    <Router>
-      <AccountProvider>
-        <LogInPage />
-      </AccountProvider>
-    </Router>
-  );
-  const email = screen.getByPlaceholderText("E-mail Address");
-  const password = screen.getByPlaceholderText("Password");
-  expect(email).toBeInTheDocument();
-  expect(password).toBeInTheDocument();
-=======
-test("renders Create Account", () => {
-  render(<LogInPage />);
-  const create = screen.getByText(/Create Account/i);
-  expect(create).toBeInTheDocument();
-
-
-});
+  })
