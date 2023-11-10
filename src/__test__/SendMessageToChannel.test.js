8b350ee7eb7a548d29049c516227c5db8d693d72
Jeffrey Biñas <binasjeffrey34@yahoo.com>
2023-11-10 19:57:10 +0800

    accept changes

--- src/__test__/SendMessageToChannel.test.js
+++ src/__test__/SendMessageToChannel.test.js
@@ -1,7 +1,8 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import SendMessageToChannel from "../components/ChannelMessages/SendMessageToChannel";
 import { AccountProvider } from "../Context/AccountContext";
+import { test, expect } from "vitest";
 
 
 test("send message to channel", () => {
