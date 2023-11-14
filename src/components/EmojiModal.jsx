883bbe5be195c974aa38310b5d04f063b4ce906c
Christian Patrick Nebab <dongyang0216@gmail.com>
2023-11-14 12:45:43 +0800

    adjust emoji modal

--- src/components/EmojiModal.jsx
+++ src/components/EmojiModal.jsx
@@ -5,7 +5,7 @@ export function EmojiModal({ inputEl }) {
   const { handleGetEmoji, handleModalEmoji } = useAccountContext();
 
   return (
-    <div className=" w-[clamp(31rem,38vw,40rem)] h-[clamp(20rem,24vw,25vh)] absolute bottom-[24%] md:bottom-[17%] left-[2%] border border-slate-300  z-20 bg-slate-50 text-3xl md:text-4xl p-4 shadow-[0_5px_1rem_rgba(0,0,0,0.10)] rounded-xl overflow-hidden">
+    <div className=" w-[clamp(30rem,38vw,40rem)] h-[clamp(20rem,24vw,25rem)] absolute bottom-[20%] left-[2.5%] border border-slate-300  z-20 bg-slate-50 text-3xl md:text-4xl p-4 shadow-[0_5px_1rem_rgba(0,0,0,0.10)] rounded-xl overflow-hidden">
       <p className="text-xl md:text-2xl mb-4 font-medium text-gray-600 border-b-[1px] border-slate-300 pb-4">
         <span>Emoji</span>
         <i
@@ -13,7 +13,7 @@ export function EmojiModal({ inputEl }) {
           onClick={() => handleModalEmoji(`is${inputEl}`)}
         ></i>
       </p>
-      <div className="emoji">
+      <div className="emoji text-2xl md:text-3xl">
         {emoji.map((emo, i) => (
           <button
             key={i}
